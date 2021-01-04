e9d5b434fb006171c1a163ac3828ad0225433d47
Igor Kolomiets <samolet@gmail.com>
2021-01-03 23:09:16 -0500

    still make GeoLocation as non-data class to exclude auxiliary population field from the rest

--- src/main/kotlin/Protokol.kt
+++ src/main/kotlin/Protokol.kt
@@ -2,13 +2,17 @@ import com.electrit.protokol.Protokol
 import com.electrit.protokol.ProtokolMapEntry
 import com.electrit.protokol.ProtokolObject
 
-data class GeoLocation(
+class GeoLocation(
     var latitude: Float = 0f,
     var longitude: Float = 0f,
     var population: Int = 0,
     var admin1: String = "",
     var admin2: String = ""
-)
+) {
+    override fun toString(): String {
+        return "GeoLocation(latitude=$latitude, longitude=$longitude, admin1='$admin1', admin2='$admin2')"
+    }
+}
 
 object GeoLocationProtokolObject : ProtokolObject<GeoLocation> {
     override fun create() = GeoLocation()
